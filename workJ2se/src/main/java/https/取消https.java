be46bf63ee995f60fea4c093ec55da73e33625d7
allen.chang <ansx86a@gmail.com>
2016-04-13 14:23:50 +0800

    完成javaMail的發送

---
+++ workJ2se/src/main/java/https/取消https.java
@@ -0,0 +1,46 @@
+package https;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class 取消https {
+
+	public static void main(String[] args) throws Exception {
+		ignoreVerifyHttpsTrustManager();
+		ignoreVerifyHttpsHostName();
+
+		
+		
+	}
+	public static void ignoreVerifyHttpsTrustManager() throws Exception {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+			}
+		} };
+
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	}
+
+	public static void ignoreVerifyHttpsHostName() {
+		HostnameVerifier hv = new HostnameVerifier() {
+			public boolean verify(String urlHostName, SSLSession session) {
+				System.out.println("Warning: URL Host: " + urlHostName + " vs. " + session.getPeerHost());
+				return true;
+			}
+		};
+		HttpsURLConnection.setDefaultHostnameVerifier(hv);
+	}
+}
---
+++ workJ2se/src/main/java/mail/Javax的mail.java
@@ -0,0 +1,74 @@
+package mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 已完成有認證和沒認證的發smtp的method
+ * 需完成html發送本文、附件、ssl(再確認要不要實作)
+ * 
+ * @author ai
+ */
+public class Javax的mail {
+
+	public static void main(String[] args) {
+		Javax的mail m = new Javax的mail();
+		// m.$001單純的smtp轉導();
+		m.$101登入的smtp轉導();
+		System.out.println("done");
+	}
+
+	public void $001單純的smtp轉導() {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "127.0.0.1");
+		props.put("mail.transport.protocol", "smtp");
+		Session session = Session.getDefaultInstance(props, null);
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("from-email@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("ansx86a@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler," + "\n\n No spam to my email, please!");
+			Transport.send(message);
+			System.out.println("Done");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void $101登入的smtp轉導() {
+		final String username = "sa@aaa.bbb";
+		final String password = "sa";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "127.0.0.1");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.transport.protocol", "smtp");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("from-email@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("ansx86a@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler," + "\n\n No spam to my email, please!");
+			Transport.send(message);
+			System.out.println("Done");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
