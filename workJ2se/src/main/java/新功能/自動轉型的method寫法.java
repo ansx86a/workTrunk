823ab0b91d5bd79c6ca5e846d5b3ea2ee854cafe
ansx86a <ansx86a@gmail.com>
2017-03-17 14:16:37 +0800

    Create 自動轉型的method寫法.java

---
+++ workJ2se/src/main/java/新功能/自動轉型的method寫法.java
@@ -0,0 +1,38 @@
+package 新功能;
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 自動轉型的method寫法 {
+
+    private Map<String, Object> attrs_ = new HashMap<>();
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        // 注意，本程式最後無法正常執行，主要是要看java語法
+        自動轉型的method寫法 t = new 自動轉型的method寫法();
+        t.addKey(ArrayList.class, "list");
+        t.addKey(HashMap.class, "map");
+        ArrayList list = t.get("list");
+        HashMap map = t.get("map");
+        System.out.println(list.getClass());
+        System.out.println(map.getClass());
+        ArrayList errorType = t.get("map");// 轉型失敗的exception
+        System.out.println(errorType.getClass());
+    }
+
+    // 這裡的<T>是吃Class<T> type，重點就擺在傳進來的type參數
+    public <T> void addKey(Class<T> type, String key) throws InstantiationException, IllegalAccessException {
+        attrs_.put(key, type.newInstance());
+    }
+
+    // 這邊要注意的是(T) attrs_.get(dest)是轉型
+    // ArrayList list = t.get("list");，public<T>的<T>是吃ArrayList list
+    // 的list，幫你轉型，有點不安全的語法
+    public <T> T get(String dest) {
+        return (T) attrs_.get(dest);
+    }
+
+}
