ccc34d280eb98d4c64f01fd261c43a5dabfec55b
ansx86a <ansx86a@gmail.com>
2019-12-31 15:31:24 +0800

    update

--- workJ2se/src/main/java/springAop/ex4/E004AspectJ.java
+++ workJ2se/src/main/java/springAop/ex4/E004AspectJ.java
@@ -1,7 +1,5 @@
 package springAop.ex4;
 
-import java.util.Arrays;
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -10,9 +8,12 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 /**
  * <pre>
@@ -21,74 +22,92 @@ import org.springframework.stereotype.Component;
  * AOP檔要上註解@Aspect+@Component
  * 注意點是Around如果把ex catch住，那AfterThrowing就不會有動作
  * </pre>
+ *
  * @author ai
  */
 @Component
 @Aspect
 public class E004AspectJ {
 
-	public static void main(String[] args) {
-		String path = "springAop/ex4/ex4.xml";
-		ApplicationContext appContext = new ClassPathXmlApplicationContext(path);
-		JustBean justBean = (JustBean) appContext.getBean("justBean");
-		justBean.say();
-		justBean.sayNo();
-		justBean.sayEx();
-		System.out.println("class end");
-	}
+    public static void main(String[] args) {
+        String path = "springAop/ex4/ex4.xml";
+        ApplicationContext appContext = new ClassPathXmlApplicationContext(path);
+        JustBean justBean = (JustBean) appContext.getBean("justBean");
+        justBean.say();
+        justBean.sayNo();
+        justBean.sayEx();
+        System.out.println("class end");
+    }
 
-	@Before("execution(* springAop.ex4.JustBean.sayYes(..))")
-	public void logBefore(JoinPoint joinPoint) {
-		System.out.print("logBefore() is running!");
-		System.out.print("\t" + joinPoint.getSignature().getName());
-		System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
-		System.out.println();
-	}
+    @Pointcut("execution(* springAop.ex4.JustBean.sayYes(..))")
+    public void exp01() {
 
-	@After("execution(* springAop.ex4.JustBean.say*(..))")
-	public void logAfter(JoinPoint joinPoint) {
+    }
 
-		System.out.print("logAfter() is running!");
-		System.out.print("\t" + joinPoint.getSignature().getName());
-		System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
-		System.out.println();
-	}
+    @Pointcut("execution(* springAop.ex4.JustBean.sayYes(..))")
+    public void exp02() {
 
-	@AfterReturning(pointcut = "execution(* springAop.ex4.JustBean.say*(..))", returning = "result")
-	// public void logAfterReturning(JoinPoint joinPoint,Object result) {
-	public void logAfterReturning(JoinPoint joinPoint, Object result) {
-		System.out.print("logAfterReturning() is running!");
-		System.out.print("\t" + joinPoint.getSignature().getName());
-		System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
-		System.out.print("\tresult=" + result);
-		System.out.println();
-	}
+    }
 
-	@AfterThrowing(pointcut = "execution(* springAop.ex4.JustBean.*(..))", throwing = "error")
-	public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
-		System.out.print("logAfterThrowing() is running!");
-		System.out.print("\t" + joinPoint.getSignature().getName());
-		System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
-		System.out.print("Exception : " + error);
-		System.out.println();
+    @AfterThrowing(pointcut = "exp01() || exp02()", throwing = "ex")
+    public void handlerEx(Throwable ex) {
+        //沒測試，但這裡是可以用pointcut來簡化寫execution(.........)
+        //throwing="ex" 不知是否影嚮到到注入的名稱
+    }
 
-	}
 
-	@Around("execution(* springAop.ex4.JustBean.*(..))")
-	public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-		System.out.print("logAround() is running!");
-		System.out.print("\t" + joinPoint.getSignature().getName());
-		System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
-		System.out.println();
-		System.out.println("logAround() before!");
-		try {
-			Object result = joinPoint.proceed(); // continue on the intercepted method
-			System.out.println("result = " + result);
-		} catch (Exception ex) {
-			System.out.println("ex:" + ex);
-			// throw ex;
-		}
-		System.out.println("logAround() end!******");
-	}
+    @Before("execution(* springAop.ex4.JustBean.sayYes(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.print("logBefore() is running!");
+        System.out.print("\t" + joinPoint.getSignature().getName());
+        System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
+        System.out.println();
+    }
+
+    @After("execution(* springAop.ex4.JustBean.say*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+
+        System.out.print("logAfter() is running!");
+        System.out.print("\t" + joinPoint.getSignature().getName());
+        System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
+        System.out.println();
+    }
+
+    @AfterReturning(pointcut = "execution(* springAop.ex4.JustBean.say*(..))", returning = "result")
+    // public void logAfterReturning(JoinPoint joinPoint,Object result) {
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        System.out.print("logAfterReturning() is running!");
+        System.out.print("\t" + joinPoint.getSignature().getName());
+        System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
+        System.out.print("\tresult=" + result);
+        System.out.println();
+    }
+
+    @AfterThrowing(pointcut = "execution(* springAop.ex4.JustBean.*(..))", throwing = "error")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
+        System.out.print("logAfterThrowing() is running!");
+        System.out.print("\t" + joinPoint.getSignature().getName());
+        System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
+        System.out.print("Exception : " + error);
+        System.out.println();
+
+    }
+
+    @Around("execution(* springAop.ex4.JustBean.*(..))")
+    public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.print("logAround() is running!");
+        System.out.print("\t" + joinPoint.getSignature().getName());
+        System.out.print("\t" + Arrays.toString(joinPoint.getArgs()));
+        System.out.println();
+        System.out.println("logAround() before!");
+        try {
+            Object result = joinPoint.proceed(); // continue on the intercepted method
+            System.out.println("result = " + result);
+        } catch (Exception ex) {
+            System.out.println("ex:" + ex);
+            // throw ex;
+        }
+        System.out.println("logAround() end!******");
+    }
 
 }
