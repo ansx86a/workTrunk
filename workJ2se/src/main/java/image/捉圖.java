70bea2a740ce93bbe5595dc7871ca8557f838272
allen <allen@gmail.com>
2023-03-29 14:34:45 +0800

    update

--- workJ2se/src/main/java/image/捉圖.java
+++ workJ2se/src/main/java/image/捉圖.java
@@ -1,68 +1,77 @@
 package image;
 
-import image.Java2D;
-
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.Transparency;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
 public class 捉圖 {
 
-	public static void main(String[] args) throws Exception {
-		BufferedImage b = 捉全圖();
-		// 這裡是適用png、bmp、jpg
-		寫檔(b, "png", "z:/aaa.png");
-		寫檔(b, "bmp", "z:/aaa.bmp");
+    public static void main(String[] args) throws Exception {
+        BufferedImage b = 捉全圖();
+        // 這裡是適用png、bmp、jpg
+        寫檔(b, "png", "z:/aaa.png");
+        寫檔(b, "bmp", "z:/aaa.bmp");
 
-		// 下面的半透明只適用png，其它的都會有問題
-		BufferedImage textPng = 取得半透明文字("什麼鬼東西");
-		寫檔(textPng, "png", "z:/什麼鬼東西.png");
-		寫檔(textPng, "jpg", "z:/什麼鬼東西.jpg");
-	}
+        // 下面的半透明只適用png，其它的都會有問題
+        BufferedImage textPng = 取得半透明文字("什麼鬼東西");
+        寫檔(textPng, "png", "z:/什麼鬼東西.png");
+        寫檔(textPng, "jpg", "z:/什麼鬼東西.jpg");
+    }
 
-	public static BufferedImage 捉全圖() throws Exception {
-		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		System.out.println(d.getHeight());
-		System.out.println(d.getWidth());
-		return 捉圖區域((int) d.getWidth(), (int) d.getHeight());
+    public static BufferedImage 捉全圖() throws Exception {
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println(d.getHeight());
+        System.out.println(d.getWidth());
+        return 捉圖區域((int) d.getWidth(), (int) d.getHeight());
 
-	}
+    }
 
-	public static BufferedImage 捉圖區域(int width, int height) throws Exception {
-		// 捉取0,0 ->寬,高的圖
-		Robot robot = new Robot();
-		BufferedImage screenImage = robot.createScreenCapture(new Rectangle(0, 0, width, height));
-		return screenImage;
-	}
+    public static BufferedImage 捉圖區域(int width, int height) throws Exception {
+        // 捉取0,0 ->寬,高的圖
+        Robot robot = new Robot();
+        BufferedImage screenImage = robot.createScreenCapture(new Rectangle(0, 0, width, height));
+        return screenImage;
+    }
 
-	public static int 取色(BufferedImage screenImage, int x, int y) {
-		int color = screenImage.getRGB(x, y);
-		System.out.println(color);
-		System.out.println(Integer.toHexString(color));
-		return color;
-	}
+    public static int 取色(BufferedImage screenImage, int x, int y) {
+        int color = screenImage.getRGB(x, y);
+        System.out.println(color);
+        System.out.println(Integer.toHexString(color));
+        return color;
+    }
 
-	public static void 寫檔(BufferedImage screenImage, String format, String path) throws Exception {
-		// formatName 填jpg、png之類的，亂填檔案出不來
-		// ImageIO.write(screenImage, "formatName", new File("e:/temptemp/xxx.png"));
-		ImageIO.write(screenImage, format, new File(path));
-	}
+    public static void 寫檔(BufferedImage screenImage, String format, String path) throws Exception {
+        // formatName 填jpg、png之類的，亂填檔案出不來
+        // ImageIO.write(screenImage, "formatName", new File("e:/temptemp/xxx.png"));
+        ImageIO.write(screenImage, format, new File(path));
+    }
 
-	public static BufferedImage 取得半透明文字(String text) throws IOException {
-		BufferedImage bim = Java2D.建立文字透明的圖(text, 600, 300, 255, 0, 0, 50);
-		return bim;
-	}
+    public static BufferedImage 取得半透明文字(String text) throws IOException {
+        BufferedImage bim = Java2D.建立文字透明的圖(text, 600, 300, 255, 0, 0, 50);
+        return bim;
+    }
+
+    public static void 全螢幕截圖() {
+        try {
+            //copy from chatgpt
+            Robot robot = new Robot();
+            String fileName = "screenshot.png";
+            File file = new File(fileName);
+            System.out.println(file.getCanonicalPath());
+
+            // Capture the whole desktop as a BufferedImage object
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+
+            // Save the captured image as a file
+            ImageIO.write(screenFullImage, "png", file);
+
+            System.out.println("Screenshot saved as " + fileName);
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
 
 }
--- workJ2se/src/main/java/tool/機器人.java
+++ workJ2se/src/main/java/tool/機器人.java
@@ -1,80 +1,106 @@
 package tool;
 
-import java.awt.AWTException;
-import java.awt.Robot;
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 /**
  * 此範例跑起來是滑鼠位移配合打字，不過中文字不行啊，只能打鍵盤上的字
- * @author ai
  *
+ * @author ai
  */
 public class 機器人 {
-	Robot robot = new Robot();
+    Robot robot = new Robot();
 
-	public static void main(String[] args) throws AWTException {
-		new 機器人();
-	}
+    public static void main(String[] args) throws AWTException {
+        new 機器人();
+    }
 
-	public 機器人() throws AWTException {
-		robot.setAutoDelay(40);
-		robot.setAutoWaitForIdle(true);
+    public 機器人() throws AWTException {
+        robot.setAutoDelay(40);
+        robot.setAutoWaitForIdle(true);
 
-		robot.delay(4000);
-		robot.mouseMove(40, 130);
-		robot.delay(500);
+        robot.delay(4000);
+        robot.mouseMove(40, 130);
+        robot.delay(500);
 
-		leftClick();
-		robot.delay(500);
-		leftClick();
+        leftClick();
+        robot.delay(500);
+        leftClick();
 
-		robot.delay(500);
-		type("Hello, world");
+        robot.delay(500);
+        type("Hello, world");
 
-		robot.mouseMove(40, 160);
-		robot.delay(500);
+        robot.mouseMove(40, 160);
+        robot.delay(500);
 
-		leftClick();
-		robot.delay(500);
-		leftClick();
+        leftClick();
+        robot.delay(500);
+        leftClick();
 
-		robot.delay(500);
-		type("This is a test of the Java Robot class");
+        robot.delay(500);
+        type("This is a test of the Java Robot class");
 
-		robot.delay(50);
-		type(KeyEvent.VK_DOWN);
+        robot.delay(50);
+        type(KeyEvent.VK_DOWN);
 
-		robot.delay(250);
-		type("Four score and seven years ago, our fathers ...");
+        robot.delay(250);
+        type("Four score and seven years ago, our fathers ...");
 
-		robot.delay(1000);
-		System.exit(0);
-	}
+        robot.delay(1000);
+        System.exit(0);
+    }
 
-	private void leftClick() {
-		robot.mousePress(InputEvent.BUTTON1_MASK);
-		robot.delay(200);
-		robot.mouseRelease(InputEvent.BUTTON1_MASK);
-		robot.delay(200);
-	}
+    private void leftClick() {
+        robot.mousePress(InputEvent.BUTTON1_MASK);
+        robot.delay(200);
+        robot.mouseRelease(InputEvent.BUTTON1_MASK);
+        robot.delay(200);
+    }
 
-	private void type(int i) {
-		robot.delay(40);
-		robot.keyPress(i);
-		robot.keyRelease(i);
-	}
+    private void type(int i) {
+        robot.delay(40);
+        robot.keyPress(i);
+        robot.keyRelease(i);
+    }
 
-	private void type(String s) {
-		byte[] bytes = s.getBytes();
-		for (byte b : bytes) {
-			int code = b;
-			// keycode only handles [A-Z] (which is ASCII decimal [65-90])
-			if (code > 96 && code < 123)
-				code = code - 32;
-			robot.delay(40);
-			robot.keyPress(code);
-			robot.keyRelease(code);
-		}
-	}
+    private void type(String s) {
+        byte[] bytes = s.getBytes();
+        for (byte b : bytes) {
+            int code = b;
+            // keycode only handles [A-Z] (which is ASCII decimal [65-90])
+            if (code > 96 && code < 123)
+                code = code - 32;
+            robot.delay(40);
+            robot.keyPress(code);
+            robot.keyRelease(code);
+        }
+    }
+
+    private void 未測測的複製貼上() {
+        String filePath = "";
+        try {
+            Robot robot = new Robot();
+            StringSelection ss = new StringSelection(filePath);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+            robot.delay(3000);
+
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.delay(3000);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            robot.delay(3000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
